3fe38e506da06ea84f8a0fc4de8fee4a7b66c59d
evemorgen <evemorgen1911@gmail.com>
2017-02-12 14:01:14 +0100

    close before open

--- gps_service/workers/gps_module_worker.py
+++ gps_service/workers/gps_module_worker.py
@@ -11,12 +11,13 @@ class GpsModuleWorker(YieldPeriodicCallback):
         self.config = Config()
         self.rx_pin = self.config['rx_pin']
         self.configure_raspi()
+        self.run_number = 1
         logging.info('starting gps worker')
         YieldPeriodicCallback.__init__(self, self.run, self.config['worker_period'], faststart=True)
 
     def configure_raspi(self):
         self.raspi = pi()
-        self.run_rumber = 1
+        self.raspi.bb_serial_read_close(self.rx_pin)
         self.raspi.set_mode(self.rx_pin, INPUT)
         self.raspi.bb_serial_read_open(self.rx_pin, 9600, 8)
         logging.info('software serial initialized')
