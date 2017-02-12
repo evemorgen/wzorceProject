83fa26f533fd930447575ec70f65f671f64a26ef
evemorgen <evemorgen1911@gmail.com>
2017-02-12 16:08:35 +0100

    parse issue

--- gps_service/utils/coords.py
+++ gps_service/utils/coords.py
@@ -10,12 +10,26 @@ class Coords():
             'status': None
         }
 
+    def _find_fields(self, frame):
+        if frame.latitude is not None:
+            self.state_dict['latitude'] = frame.latitude
+        if frame.latitude is not None:
+            self.state_dict['latitude_minutes'] = frame.latitude_minutes
+        if frame.latitude_seconds is not None:
+            self.state_dict['latitude_seconds'] = frame.latitude_seconds
+        if frame.latitude is not None:
+            self.state_dict['longitude'] = frame.longitude
+        if frame.latitude is not None:
+            self.state_dict['longitude_minutes'] = frame.longitude_minutes
+        if frame.latitude_seconds is not None:
+            self.state_dict['longitude_seconds'] = frame.longitude_seconds
+
+
     def add_frame(self, frame):
         try:
             parsed_frame = pynmea2.parse(frame)
             print(dir(parsed_frame))
-            #if parsed_frame.status is not None:
-            #    self.state_dict['status'] = parsed_frame.status
+            self._find_fields()
             self.frames_list.append(parsed_frame)
             self.frames_list = self.frames_list[-100:]
         except pynmea2.ParseError:
