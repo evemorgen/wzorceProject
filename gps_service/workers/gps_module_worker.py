e5f704a52bf58a8f0b8d58e549e2d9047045397a
evemorgen <evemorgen1911@gmail.com>
2017-02-12 14:17:07 +0100

    debug inside worker 4

--- gps_service/workers/gps_module_worker.py
+++ gps_service/workers/gps_module_worker.py
@@ -26,7 +26,7 @@ class GpsModuleWorker(YieldPeriodicCallback):
     def run(self):
         (count, data) = self.raspi.bb_serial_read(self.rx_pin)
         data = data.decode('utf-8')
-        print('%s %s' % (data[0] == '$', data[-3]))
         if count > 0:
-            print("-*-\n%d\n\n%s\n-#-" % (self.run_number, data))
+            ramki = data.split('\n')
+            print("-*-\n%d\n\n%s\n-#-" % (self.run_number, ramki))
         self.run_number += 1
