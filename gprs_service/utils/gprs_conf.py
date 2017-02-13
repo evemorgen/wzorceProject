9d471276c5ae586913b36980cdfaa36615837c6e
szmo <szymonduda10@gmail.com>
2017-02-14 00:39:28 +0100

    gprs config syntax fix

--- gprs_service/utils/gprs_conf.py
+++ gprs_service/utils/gprs_conf.py
@@ -1,33 +1,36 @@
 import serial
 import time
 
+class Gprs_conf():
+    def __init__(self):
+        console = serial.Serial(
+            port='/dev/ttyAMA0',
+            baudrate=9600,
+        )
 
-def reader(t):
-    out = ''
-    time.sleep(t)
-    while console.inWaiting() > 0:
-        out += console.read(1)
-    if out != '':
-        print ">>" + out
+    def reader(self,t):
+        out = ''
+        time.sleep(t)
+        while console.inWaiting() > 0:
+            out += console.read(1)
+        if out != '':
+            print ">>" + out
+
+    def setConf(self):    
+        self.console.isOpen()
+        time.sleep(1)
+        self.console.write('at+cfun=1,1\r\n')
+        self.reader(1)
+        self.console.write('at+creg=1\r\n')
+        self.reader(1)
+        self.console.write('at+cgatt=1\r\n')
+        self.reader(1)
+        self.console.write('at+cstt="internet.cp","",""\r\n')
+        self.reader(1)
+        self.console.write('at+ciicr\r\n')
+        self.reader(1)
+        self.console.write('at+cifsr\r\n')
 
 
-def gprs_conf():
-    console = serial.Serial(
-        port='/dev/ttyAMA0',
-        baudrate=9600,
-    )
-    console.isOpen()
-    time.sleep(1)
-    console.write('at+cfun=1,1\r\n')
-    reader(1)
-    console.write('at+creg=1\r\n')
-    reader(1)
-    console.write('at+cgatt=1\r\n')
-    reader(1)
-    console.write('at+cstt="internet.cp","",""\r\n')
-    reader(1)
-    console.write('at+ciicr\r\n')
-    reader(1)
-    console.write('at+cifsr\r\n')
-
-gprs_conf()
+conf = Gprs_conf()
+conf.setConf()
