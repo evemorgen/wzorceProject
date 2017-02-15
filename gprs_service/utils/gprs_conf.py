cfddd10cfed9ca039eb0b321cec4a771b4a2e742
szmo <szymonduda10@gmail.com>
2017-02-15 13:10:52 +0100

    aaaa jebac

--- gprs_service/utils/gprs_conf.py
+++ gprs_service/utils/gprs_conf.py
@@ -1,35 +1,38 @@
 import serial
 import time
-import serial_init
+
 
 class Gprs_conf():
 
     def __init__(self):
-        self.ser = Serial_init.console
+        self.console = serial.Serial(
+        port='/dev/ttyAMA0',
+        baudrate=9600,
+    )
 
     def reader(self,t):
         out = ''
         time.sleep(t)
-        while self.ser.inWaiting() > 0:
-            out += self.ser.read(1)
+        while self.console.inWaiting() > 0:
+            out += self.console.read(1)
         if out != '':
             print ">>" + out
         time.sleep(10)
 
     def setConf(self):    
-        self.ser.isOpen()
+        self.console.isOpen()
         time.sleep(1)
-        self.ser.write('at+cfun=1,1\r\n')
+        self.console.write('at+cfun=1,1\r\n')
         self.reader(2)
-        self.ser.write('at+creg=1\r\n')
+        self.console.write('at+creg=1\r\n')
         self.reader(2)
-        self.ser.write('at+cgatt=1\r\n')
+        self.console.write('at+cgatt=1\r\n')
         self.reader(2)
-        self.ser.write('at+cstt="internet.cp","",""\r\n')
+        self.console.write('at+cstt="internet.cp","",""\r\n')
         self.reader(2)
-        self.ser.write('at+ciicr\r\n')
+        self.console.write('at+ciicr\r\n')
         self.reader(2)
-        self.ser.write('at+cifsr\r\n')
+        self.console.write('at+cifsr\r\n')
         self.reader(2)
 
 
