03c6d5690e2428fcde0953c1eb37f990e8bb7264
szmo <szymonduda10@gmail.com>
2017-02-15 12:53:58 +0100

    test

--- gprs_service/utils/serial_init.py
+++ gprs_service/utils/serial_init.py
@@ -2,9 +2,8 @@ import serial
 
 
 class Serial_init:
-    def __init__(self):
-        self.console = serial.Serial(
-            port='/dev/ttyAMA0',
-            baudrate=9600,
-        )
+    console = serial.Serial(
+        port='/dev/ttyAMA0',
+        baudrate=9600,
+    )
 
