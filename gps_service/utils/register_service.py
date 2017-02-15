f04cefa3d59d2cb494558e730e4b5d40f82491e7
evemorgen <evemorgen1911@gmail.com>
2017-02-16 00:12:35 +0100

    register/unregister call

--- gps_service/utils/register_service.py
+++ gps_service/utils/register_service.py
@@ -1,22 +1,35 @@
+import json
 import os
 from tornado.httpclient import AsyncHTTPClient
 
 
 class Service():
-    def __init__(self, host, port, ifc_list):
+    def __init__(self, host, port, ifc, methods):
+        self.ifc_id = None
         self.host = host
         self.port = port
-        self.ifc_list = ifc_list
+        self.ifc = ifc
+        self.methods = methods
         self.observer_host = os.environ['OBSERVER_IP']
         self.observer_port = os.environ['OBSERVER_PORT']
         self.http_client = AsyncHTTPClient()
 
     def get_id(self, response):
-        print(response)
+        print(response.body)
 
     def register_interface(self):
-        self.http_client.fetch("http://www.google.com/", self.get_id)
+        feed = {
+            "ip": self.host,
+            "port": self.port,
+            "name": self.ifc,
+            "methods": self.methods
+        }
+        self.http_client.fetch("http://%s:%s/register" % (self.observer_host, self.observer_port), self.get_id, method='POST', body=json.dumps(feed), headers={'Content-type': 'application/json'})
 
     def unregister_interface(self):
         if self.ifc_id is None:
             raise Exception('Cannot unregister, interface not registred')
+        feed = {
+            "id": self.ifc_id
+        }
+        self.http_client.fetch("http://%s:%s/unregister" % (self.observer_host, self.observer_port), self.unreg_result, method='POST', body=json.dumps(feed), headers={'Content-type': 'application/json'})
