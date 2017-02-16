7eac7ab82b94840fd38b5f977fe47dd996fe0ed7
karopyszcz <karopyszcz@gmail.com>
2017-02-16 12:19:47 +0100

    /unregister as POST

--- src/main/java/apka/controllers/UnregisterController.java
+++ src/main/java/apka/controllers/UnregisterController.java
@@ -1,10 +1,13 @@
 package apka.controllers;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import apka.services.UnregisterService;
@@ -16,8 +19,8 @@ public class UnregisterController {
 	@Autowired
 	private UnregisterService unregisterService;
 	
-	@RequestMapping(method = RequestMethod.GET)
-    public @ResponseBody String unregister(@RequestParam(value="id") String id) {
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody String unregister(@RequestBody Map<String, String> id) {
         return unregisterService.unregister(id);
     }
 }
--- src/main/java/apka/services/UnregisterService.java
+++ src/main/java/apka/services/UnregisterService.java
@@ -1,13 +1,19 @@
 package apka.services;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 import apka.Configuration;
 
 @Service
 public class UnregisterService {
+	
+	private String id;
+	
+	public String unregister(Map<String, String> id_json){
 		
-	public String unregister(String id){
+		id = id_json.get("id");
 		Configuration.getInstance().deleteClient(id);
 		Configuration.getInstance().saveJson();
 		return "OK";
