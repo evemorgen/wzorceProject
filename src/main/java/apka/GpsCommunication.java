bffc763a61b500b4228ed24a949b958b8dd31bda
MICHALLL <micher.pl@gmail.com>
2017-02-18 00:36:26 +0100

    Update GpsCommunication class

--- src/main/java/apka/GpsCommunication.java
+++ src/main/java/apka/GpsCommunication.java
@@ -10,30 +10,31 @@ import java.util.List;
  */
 public class GpsCommunication {
 
-    private String url;
+    private final String get_sat_info_message = "{\"method\": \"get_sat_info\"}";
+    private final String get_pos_info_message = "{\"method\": \"get_pos_info\"}";
+    private final String get_all_data_message = "{\"method\": \"get_all_data\"}";
+    private final String get_raw_frames_message = "{\"method\": \"get_raw_frames\",";
+
     RestTemplate restTemplate = new RestTemplate();
 
-    public  GpsCommunication(String url){
-        this.url = url;
+
+    public Healthcheck healthcheck(String url){
+        return restTemplate.postForObject(url, null, Healthcheck.class);
     }
 
-    public Healthcheck healthcheck(String message){
-        return restTemplate.postForObject(url, message,Healthcheck.class);
+    public GetSatInfo get_sat_info(String url){
+        return restTemplate.postForObject(url, get_sat_info_message,GetSatInfo.class);
     }
 
-    public GetSatInfo getSatInfo(String message){
-        return restTemplate.postForObject(url, message,GetSatInfo.class);
+    public GetPosInfo get_pos_info(String url){
+        return restTemplate.postForObject(url, get_pos_info_message,GetPosInfo.class);
     }
 
-    public GetPosInfo getPosInfo(String message){
-        return restTemplate.postForObject(url, message,GetPosInfo.class);
+    public GetAllData get_all_data(String url){
+        return restTemplate.postForObject(url, get_all_data_message,GetAllData.class);
     }
 
-    public GetAllData getAllData(String message){
-        return restTemplate.postForObject(url, message,GetAllData.class);
-    }
-
-    public List getRawFrames(String message){
-        return restTemplate.postForObject(url, message, List.class);
+    public List get_raw_frames(int n, String url){
+        return restTemplate.postForObject(url, get_raw_frames_message+" \""+n+"\": 10}", List.class);
     }
 }
