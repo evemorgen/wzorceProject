d654922d74c5e49c6c534d0e27eb399839c64030
karopyszcz <karopyszcz@gmail.com>
2017-02-18 14:19:15 +0100

    HealhCheck every 30sec

---
+++ src/main/java/apka/CheckModules.java
@@ -0,0 +1,19 @@
+package apka;
+
+import java.util.TimerTask;
+
+import org.springframework.web.client.RestTemplate;
+
+public class CheckModules extends TimerTask {
+
+	public void run() {
+		RestTemplate rest = new RestTemplate();
+		for (Client client : Configuration.getInstance().getCfg()) {
+			try {
+				String temp = rest.postForObject(client.getIp() + ":" + client.getPort(), null, String.class);
+			} catch (Exception e) {
+				Configuration.getInstance().deleteClient(client.getId());
+			}
+		}
+	}
+}
--- src/main/java/apka/Main.java
+++ src/main/java/apka/Main.java
@@ -1,13 +1,16 @@
 package apka;
 
+import java.util.Timer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
+		Timer timer = new Timer();
+		timer.schedule(new CheckModules(), 0, 30000);
 	}
 }
