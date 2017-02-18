61164226247d6807e19f14f47af0a108e21313ea
evemorgen <evemorgen1911@gmail.com>
2017-02-18 16:33:41 +0100

    restTemplate proxy request

--- src/main/java/apka/services/RequestService.java
+++ src/main/java/apka/services/RequestService.java
@@ -1,6 +1,9 @@
 package apka.services;
 
+import org.apache.log4j.Logger;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,16 +15,21 @@ import apka.RequestModel;
 @Service
 public class RequestService {
 
+        static Logger log = Logger.getLogger(RequestService.class.getName());
 	private String ip;
 	private Integer port;
 	private String url;
 	private String data;
-	
+        private String payload;
+        private RestTemplate restTemplate = new RestTemplate();
+        private String result;
+
 	public String request(RequestModel requestModel) {
 		for (Client client : Configuration.getInstance().getCfg()) {
 			if (client.getName().equals(requestModel.getService())) {
-				this.ip = client.getIp();
-				this.port = client.getPort();
+                                log.info(String.format("found service %s on %s:%s", client.getName(), client.getIp(), client.getPort()));
+				ip = client.getIp();
+				port = client.getPort();
 			}
 		}
 		try {
@@ -29,9 +37,16 @@ public class RequestService {
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
-		url = ip + ":" + port;
-		return "{\"url\":\"" + url + "\", \"method\": \"" + requestModel.getMethod() + ", \"data\": " + data + "}";
+		url = "http://" + ip + ":" + port + "/";
+                if(data.equals("{}")) {
+                    payload = String.format("{\"method\": \"%s\"}", requestModel.getMethod());
+                } else {
+                    payload = String.format("{\"method\": \"%s\", %s}", requestModel.getMethod(), data.substring(1, data.length()-1));
+                }
+                String result = restTemplate.postForObject(url, payload, String.class);
+                log.info("got result: " + result);
 
+                return result;
 	}
 
 }
