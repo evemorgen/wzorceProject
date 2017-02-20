e8a3a9a5a51cd62b27e7315bf7cd6cf26ea80277
MICHALLL <micher.pl@gmail.com>
2017-02-20 12:00:59 +0100

    Fix

--- src/main/java/apka/ServicesHandling.java
+++ src/main/java/apka/ServicesHandling.java
@@ -17,26 +17,26 @@ public class ServicesHandling extends TimerTask {
 	@Value("${gateway.request}")
 	String urlRequest;
 	String url = "http://"+ip+":"+port+"/"+urlRequest;
+	private static int idSurvey = 0;
 	private ArrayList<GetAllData> getAllDataArrayList = new ArrayList<GetAllData>();
 
 	public void run() {
 		RestTemplate rest = new RestTemplate();
-		for (Client client : Configuration.getInstance().getCfg()) {
-			if (client.getName().equals("GPS")){
-				String getAllData_message = "{\"service\": \""+client.getName()+"\", \"method\": \""+client.getMethod()+"}";
+		for (Service service : Configuration.getInstance().getCfg()) {
+			if (service.getName().equals("GPS")){
+				String getAllData_message = "{\"service\": \""+ service.getName()+"\", \"method\": \""+ service.getMethod()+"}";
 				getAllDataArrayList.add(rest.postForObject(url, getAllData_message, GetAllData.class));
 			}
-			if (client.getName().equals("external")){
+			if (service.getName().equals("external")){
 				GetAllData lastAllDataInfo = getAllDataArrayList.remove(getAllDataArrayList.size()-1);
-				String uri = client.getIp() + ":" + client.getPort()+"/real_data/{lat}/{lon}/{line}/{ts}/{id}";
+				String uri = service.getIp() + ":" + service.getPort()+"/real_data/{lat}/{lon}/{line}/{ts}/{id}";
 				Map<String,String> params = new HashMap<String,String>();
 				params.put("lat", String.valueOf(lastAllDataInfo.getPos().getLatitude()));
 				params.put("lon", String.valueOf(lastAllDataInfo.getPos().getLongitude()));
-				//TODO
 				params.put("line", "19");
 				params.put("ts", lastAllDataInfo.getTimestamp());
-				//TODO
-				params.put("id", "1");
+				params.put("id", String.valueOf(idSurvey));
+				++idSurvey;
 				rest.getForEntity(uri, null, params);
 			}
 		}
