8e7b9c4e84ccccc296e2fdc06d03c6de09e985c7
Patryk Gałczyński <pgalczynski@dreamlab.pl>
2017-02-20 22:23:19 +0100

    moving stuff around

---
+++ gateway/src/main/java/gateway/services/MethodsService.java
@@ -0,0 +1,31 @@
+package gateway.services;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import gateway.config.Configuration;
+import gateway.model.Client;
+
+@Service
+public class MethodsService {
+
+	static Logger log = Logger.getLogger(MethodsService.class.getName());
+	
+	public String methods(String name) {
+		for (Client client : Configuration.getInstance().getCfg()) {
+			if (client.getName().equals(name)) {
+				try {
+					String result = "{\"methods\":" + new ObjectMapper().writeValueAsString(client.getMethods()) + "}";
+					log.info("got result: " + result);
+					return result;
+				} catch (JsonProcessingException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return "SERVICE NOT FOUND";
+	}
+}
---
+++ gateway/src/main/java/gateway/services/RegisterService.java
@@ -0,0 +1,44 @@
+package gateway.services;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import gateway.config.Configuration;
+import gateway.model.Client;
+
+@Service
+public class RegisterService {
+	
+	private Client client;
+	
+	public String register(String registrationTO){
+		try {
+			client = new ObjectMapper().readValue(registrationTO, Client.class);
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		client.setId(this.md5());
+		Configuration.getInstance().setClient(client);
+		Configuration.getInstance().saveJson();
+		return "{\"id\":\"" + client.getId() + "\"}";
+	}
+	
+	private String md5(){
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			String id = "xyz" + System.currentTimeMillis();
+	        byte[] messageDigest = md.digest(id.getBytes());
+	        BigInteger number = new BigInteger(1, messageDigest);
+	        return number.toString(16);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
